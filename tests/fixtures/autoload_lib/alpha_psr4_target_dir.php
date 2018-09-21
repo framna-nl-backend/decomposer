a3e70bbf9b4b82252e3114caa904a1944fa0ae36
Damien Tardy-Panis <damien@m2mobi.com>
2018-09-21 09:54:10 +0200

    Revert "add tests for target-dir field and fix its usage"
    This reverts commit 8ebd93af8672d2e21c72ec19282d12d3c86d7fc0.

--- tests/fixtures/autoload_lib/alpha_psr4_target_dir.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-autoload_register_psr4_prefix('alpha-lib', 'Alpha-1.0/Target/src/alpha/');
-
-?>
--- tests/fixtures/autoload_lib/beta_psr0_target_dir.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-set_include_path(
-    get_include_path() . ':' .
-    __DIR__ . '/Beta-1.0/Target/src/beta/'
-);
-
-?>
